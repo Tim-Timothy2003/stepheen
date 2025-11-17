05e64bd436a067e67f5399a50e4ddff707853f15
Tim-Timothy2003 <timothysteve042@gmail.com>
2025-11-17 12:08:57 +0100

    firts commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const Hambuger = document.getElementByid ("Hambuger");
+const klc = document.getElementById ("klc");
+
+Hambuger.addEventListener ("click", ( a>
+     klc.classList.toggle("active")
+))
